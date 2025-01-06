944a315bf27b2fe14250d553262659264f93e8cd
Anthonin <akadi@artfx.fr>
2025-01-06 10:47:52 +0100

    [Build] CE que tu veux

--- out/build/x64-Debug/_deps/sdl3-build/include-revision/SDL3/SDL_revision.h
+++ out/build/x64-Debug/_deps/sdl3-build/include-revision/SDL3/SDL_revision.h
@@ -31,9 +31,9 @@
 /* #undef SDL_VENDOR_INFO */
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL3-7983094 (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL3-7a6c0c2 (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL3-7983094"
+#define SDL_REVISION "SDL3-7a6c0c2"
 #endif
 
 #endif /* SDL_revision_h_ */
