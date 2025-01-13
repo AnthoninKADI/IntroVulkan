7c7e359cc40a6d64a8b684c39f3b0c600ad500c1
Anthonin <akadi@artfx.fr>
2025-01-13 13:12:39 +0100

    [Add] Video Rotation

--- out/build/x64-Debug/_deps/sdl3-build/include-revision/SDL3/SDL_revision.h
+++ out/build/x64-Debug/_deps/sdl3-build/include-revision/SDL3/SDL_revision.h
@@ -31,9 +31,9 @@
 /* #undef SDL_VENDOR_INFO */
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL3-723b8d3 (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL3-225d3e9 (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL3-723b8d3"
+#define SDL_REVISION "SDL3-225d3e9"
 #endif
 
 #endif /* SDL_revision_h_ */
